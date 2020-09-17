763159a1a7f7a8fab18ca79625692b0587ecf687
root <lzj01071508@buaa.edu.cn>
2020-09-17 23:30:17 +0800

    finish project set 0

--- junior1/os/cs140-ps0/caltrain.c
+++ junior1/os/cs140-ps0/caltrain.c
@@ -1,10 +1,13 @@
 #include "pintos_thread.h"
 
 struct station {
-	struct lock *lock;
-	struct condition *cond; 
+	struct lock lock;
+	struct condition cond_train_arrive; 
+	struct condition cond_passenger_seated;
 	int total_passengers;
 	int total_seats;
+	int total_in_wait;
+	int total_on_board;
 };
 
 void
@@ -12,32 +15,44 @@ station_init(struct station *station)
 {
 	station->total_passengers = 0;
 	station->total_seats = 0;
-	lock_init(station->lock);
-	cond_init(station->cond);
+	station->total_in_wait = 0;
+	lock_init(&station->lock);
+	cond_init(&station->cond_train_arrive);
+	cond_init(&station->cond_passenger_seated);
 }
 
 void
 station_load_train(struct station *station, int count)
 {
-	lock_acquire(station->lock);
+	lock_acquire(&station->lock);
 	station->total_seats = count;
-	lock_release(station->lock);
-	cond_broadcast(station->cond, station->lock);
+	while (station->total_on_board < station->total_seats && station->total_passengers) {
+		cond_broadcast(&station->cond_train_arrive, &station->lock);
+		cond_wait(&station->cond_passenger_seated, &station->lock);
+	}
+	station->total_in_wait = 0;
+	station->total_seats = 0;
+	station->total_on_board = 0;
+	lock_release(&station->lock);
 }
 
 void
 station_wait_for_train(struct station *station)
 {
-	lock_acquire(station->lock);
+	lock_acquire(&station->lock);
 	station->total_passengers++;
-	while (!station->total_seats)
-		cond_wait(station->cond, station->lock);
-	station->total_seats--;
-	lock_release(station->lock);
+	while (station->total_seats <= station->total_in_wait)
+		cond_wait(&station->cond_train_arrive, &station->lock);
+	station->total_in_wait++;
+	lock_release(&station->lock);
 }
 
 void
 station_on_board(struct station *station)
 {
-	// FILL ME IN
+	lock_acquire(&station->lock);
+	station->total_passengers--;
+	station->total_on_board++;
+	cond_signal(&station->cond_passenger_seated, &station->lock);
+	lock_release(&station->lock);
 }
--- junior1/os/cs140-ps0/reaction.c
+++ junior1/os/cs140-ps0/reaction.c
@@ -6,23 +6,56 @@
 void make_water();
 
 struct reaction {
-	// FILL ME IN
+	struct lock lock;
+	struct condition cond_o;
+	struct condition cond_h2o;
+	int total_h;
 };
 
 void
 reaction_init(struct reaction *reaction)
 {
-	// FILL ME IN
+	lock_init(&reaction->lock);
+	cond_init(&reaction->cond_o);
+	cond_init(&reaction->cond_h2o);
+	reaction->total_h = 0;
 }
 
 void
 reaction_h(struct reaction *reaction)
 {
-	// FILL ME IN
+	lock_acquire(&reaction->lock);
+	reaction->total_h++;
+	cond_signal(&reaction->cond_o, &reaction->lock);
+	cond_wait(&reaction->cond_h2o, &reaction->lock);
+
+	lock_release(&reaction->lock);
 }
 
 void
 reaction_o(struct reaction *reaction)
 {
-	// FILL ME IN
+	lock_acquire(&reaction->lock);
+	while (reaction->total_h < 2) {
+		cond_wait(&reaction->cond_o, &reaction->lock);
+	}
+	reaction->total_h -= 2;
+	cond_signal(&reaction->cond_h2o, &reaction->lock);
+	cond_signal(&reaction->cond_h2o, &reaction->lock);
+	make_water();
+	lock_release(&reaction->lock);
+	// lock_acquire(&reaction->lock);
+	// reaction->total_o++;
+	// if (reaction->total_h >= 2) {
+	// 	cond_signal(&reaction->cond_1h_wait, &reaction->lock);
+	// 	cond_signal(&reaction->cond_2h_wait, &reaction->lock);
+
+	// } else {
+	// 	while (reaction->total_h < 2) 
+	// 		cond_wait(&reaction->cond_o_wait, &reaction->lock);
+	// }
+	// make_water();
+	// printf("h: %d\n", reaction->total_o);
+	// reaction->total_o--;
+	// lock_release(&reaction->lock);	
 }
